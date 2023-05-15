06695d131741daa1dd739366cc8577c41c843777
eddmann <the@eddmann.com>
2023-05-15 21:08:57 +0000

    Deploying to gh-pages from @ eddmann/santa-lang-rs@3a0609a42bd495d380846a688677fa1390cb9a25 🚀

--- assets/santa_lang_bg.wasm.d.ts
+++ assets/santa_lang_bg.wasm.d.ts
@@ -6,5 +6,5 @@ export function aoc_test(a: number, b: number, c: number, d: number): void;
 export function evaluate(a: number, b: number, c: number, d: number): void;
 export function __wbindgen_export_0(a: number): number;
 export function __wbindgen_export_1(a: number, b: number, c: number): number;
-export function __wbindgen_add_to_stack_pointer(a: number): number;
 export function __wbindgen_export_2(a: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
